623544e46c1a3675052632c63c77ccf8f6ca1f73
qingyiwebt <qingyiweb@gmail.com>
2021-07-17 00:10:35 +0900

    support ret instruction

---
+++ grasslang.OwoVMCompiler/CodeBuffer.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace grasslang.OwoVMCompiler
+{
+    public record Register(byte index, ImmediateType type = ImmediateType.Int32);
+    public enum ImmediateType
+    {
+        Uint64, Uint32, Uint16, Uint8, Int64, Int32, Int16, Int8, Float, Double,
+        Unknown
+    }
+    public record Immediate(ImmediateType type, string value);
+    public class CodeBuffer
+    {
+        public List<byte> Buffer = new List<byte>();
+        public int Length => Buffer.Count;
+        public void Emit(byte a)
+        {
+            Buffer.Add(a);
+        }
+        public void Emit(byte[] a)
+        {
+            Buffer.AddRange(a);
+        }
+        public void Emit(string a)
+        {
+            Emit(Encoding.Default.GetBytes(a));
+        }
+        public void Emit(UInt64 a)
+        {
+            Buffer.AddRange(BitConverter.GetBytes(a));
+        }
+        public void Emit(Int64 a)
+        {
+            Buffer.AddRange(BitConverter.GetBytes(a));
+        }
+        public void Emit(Register a)
+        {
+            Emit(a.index);
+        }
+        public void Emit(Immediate a)
+        {
+            if (a.type is ImmediateType.Uint64
+                or ImmediateType.Uint32
+                or ImmediateType.Uint16
+                or ImmediateType.Uint8)
+            {
+                Emit(ulong.Parse(a.value));
+            } else if (a.type is ImmediateType.Int64
+                or ImmediateType.Int32
+                or ImmediateType.Int16
+                or ImmediateType.Int8)
+            {
+                Emit(long.Parse(a.value));
+            } else if (a.type is ImmediateType.Double)
+            {
+                Emit(BitConverter.GetBytes(double.Parse(a.value)));
+            } else if (a.type is ImmediateType.Float)
+            {
+                Emit(BitConverter.GetBytes(double.Parse(a.value)));
+            }
+        }
+        public void Emit(CodeBuffer a)
+        {
+            Buffer.AddRange(a.Buffer);
+        }
+        public byte[] Build()
+        {
+            return Buffer.ToArray();
+        }
+
+        private static Dictionary<ImmediateType, byte> typeBitsMap = new Dictionary<ImmediateType, byte>
+        {
+            {ImmediateType.Int8, 0b0100},
+            {ImmediateType.Int16, 0b0100},
+            {ImmediateType.Int32, 0b0100},
+            {ImmediateType.Int64, 0b0100},
+            {ImmediateType.Uint8, 0b0000},
+            {ImmediateType.Uint16, 0b0000},
+            {ImmediateType.Uint32, 0b0000},
+            {ImmediateType.Uint64, 0b0000},
+
+            {ImmediateType.Float, 0b1000},
+            {ImmediateType.Double, 0b1100},
+            {ImmediateType.Unknown, 0b0000 }
+        };
+        private static byte GetTypeBits(ImmediateType type, bool isImm = false)
+        {
+            byte raw = typeBitsMap[type];
+            if (raw == 0) return raw;
+            raw = (byte)(raw | (isImm ? 0b0010 : 0b0001));
+            return raw;
+        }
+
+        public void EmitMov<T>(Register reg, T val)
+        {
+            Emit((byte)0x0D); // opcode: mov
+            Emit(reg); // op1
+            if(val is Register op2reg)
+            {
+                Emit(op2reg); // op2
+                Emit((byte)((GetTypeBits(op2reg.type) << 4) | GetTypeBits(op2reg.type))); // opext
+            } else if(val is Immediate op2imm)
+            {
+                Emit((byte)0); // op2
+                Emit((byte)((GetTypeBits(op2imm.type) << 4) | GetTypeBits(op2imm.type, true))); // opext
+                Emit(op2imm);
+            }
+        }
+
+        public void EmitVMCall()
+        {
+            Emit(new byte[] { 0x0E, 0, 0, 0 });
+        }
+
+        public void EmitString(Register reg, string str)
+        {
+            Emit((byte)0x12); // opcode: const string
+            Emit(reg); // op1
+            Emit(0); // op2
+            Emit(0); // opext
+            Emit(Convert.ToUInt64(str.Length)); // string length
+            Emit(str); // string
+        }
+
+        public void EmitRet()
+        {
+            Emit((byte)0x0B); // opcode: ret
+            Emit(0); // op1
+            Emit(0); // op2
+            Emit(0); // opext
+        }
+    }
+}
--- grasslang.OwoVMCompiler/Compiler.cs
+++ grasslang.OwoVMCompiler/Compiler.cs
@@ -10,37 +10,63 @@ namespace grasslang.OwoVMCompiler
     {
         List<IndexItem> indexItems = new List<IndexItem>();
         List<VariableInfo> variables = new List<VariableInfo>();
-        uint TopRegister = 0; // r0-31为变量寄存器（超出则压入栈中），r32-47计算临时寄存器，r48-61为参数寄存器（超出压栈），r62为参数数量，r63为返回值
-        uint TempRegister = 32;
+        
         public bool enableVMCall = false;
+        public CodeBuffer codeBuffer = new CodeBuffer();
+        private static Dictionary<string, ImmediateType> immTypes = new Dictionary<string, ImmediateType>();
+        private static ImmediateType GetImmediateType(string typeString, string valueString = null)
+        {
+            if(!immTypes.ContainsKey(typeString))
+            {
+                if(!string.IsNullOrEmpty(valueString))
+                {
+                    return valueString.Contains('.') ? ImmediateType.Float : ImmediateType.Int32;
+                }
+                return ImmediateType.Int32;
+            }
+            return immTypes[typeString];
+        }
+        private void moveVariable(byte index, string name, ref CodeBuffer buffer)
+        {
+            var resultVars = (from var in variables where var.name == name select var);
+            if (!resultVars.Any())
+            {
+                throw new Exception("Undefined identifier: " + name);
+            }
+            var inputVar = resultVars.First();
+            buffer.EmitMov(
+                new Register(index),
+                new Register((byte)inputVar.register, GetImmediateType(inputVar.type))
+                );
+        }
+        private CodeBuffer compileNode(Node node, uint startReg = 0, uint startTempReg = 32)
+        {
+            uint TopRegister = startReg; // r0-31为变量寄存器（超出则压入栈中），r32-47计算临时寄存器，r48-61为参数寄存器（超出压栈），r62为参数数量，r63为返回值
+            uint TempRegister = startTempReg;
+            CodeBuffer result = new CodeBuffer();
 
-        private static byte[] instMovToRegister(byte reg,UInt64 value)
-        {
-            var result = new byte[] { 0x0D, reg, 0, 0b01010110 }.ToList();
-            result.AddRange(BitConverter.GetBytes(value));
-            return result.ToArray();
-        }
-        private static byte[] instMovToRegister(byte reg, byte value)
-        {
-            return new byte[] { 0x0D, reg, value, 0b01010101 };
-        }
-        private byte[] compileNode(Node node)
-        {
-            List<byte> result = new List<byte>();
+
             if (node is LetStatement letStatement)
             {
                 var definition = letStatement.Definition;
                 variables.Add(new VariableInfo(definition.Name.Literal, definition.ObjType.Literal, TopRegister));
                 if (definition.Value is NumberLiteral numberLiteral)
                 {
-                    byte type = (byte)(numberLiteral.Value.Contains(".") ? 0b01011010 : 0b01010110);
-                    result.AddRange(new byte[] { 0x0D, (byte)TopRegister, 0x00, type });
-                    var value = BitConverter.GetBytes(long.Parse(numberLiteral.Value));
-                    result.AddRange(value);
-                } else if (definition.Value is StringLiteral)
+                    // rvalue is number
+                    var rvalue = numberLiteral.Value;
+                    var type = definition.ObjType.Literal;
+
+                    result.EmitMov(
+                        new Register((byte)TopRegister), 
+                        new Immediate(GetImmediateType(type, rvalue), numberLiteral.Value)
+                    );
+                } else if (definition.Value is IdentifierExpression identifierExpression)
                 {
-                    result.AddRange(compileNode(definition.Value));
-                    result.AddRange(new byte[] { 0x0D, (byte)TopRegister, (byte)TempRegister, 00}); // mov堆上对象不需要type
+                    moveVariable((byte)TopRegister, identifierExpression.Literal, ref result);
+                } else if (definition.Value is StringLiteral stringLiteral)
+                {
+                    result.Emit(compileNode(stringLiteral, TopRegister, TempRegister));
+                    result.EmitMov(new Register((byte)TopRegister), new Register((byte)TempRegister, ImmediateType.Unknown));
                 }
                 else
                 {
@@ -52,133 +78,72 @@ namespace grasslang.OwoVMCompiler
             else if (node is ExpressionStatement expressionStatement)
             {
                 return compileNode(expressionStatement.Expression);
-            }
-            else if (node is AssignExpression assignExpression)
-            {
-                if (assignExpression.Left is IdentifierExpression)
-                {
-                    var varname = assignExpression.Left.Literal;
-                    var dsts = (from var in variables where var.name == varname select var);
-                    if (!dsts.Any())
-                    {
-                        throw new Exception("Undefined variable: " + varname);
-                    }
-                    // 思路：先计算右值，最后将计算结果的临时寄存器mov回去
-                    result.AddRange(compileNode(assignExpression.Right));
-                    result.AddRange(new byte[] { 0x0D, (byte)dsts.First().register, (byte)TempRegister, 0b01010101 });
-                }
-            }
-            else if (node is InfixExpression infixExpression)
-            {
-                // 思路：将第一个操作数mov到第一个临时寄存器，第二个操作数mov到第二个临时寄存器，结果为第一个临时寄存器
-
-                // mov t1, op1
-                uint lastTempRegister = TempRegister;
-                Action<Expression> movIntoTempRegister = (expression) =>
-                {
-                    result.Add((byte)0x0D);
-                    result.Add((byte)TempRegister);
-                    if (expression is IdentifierExpression identifierExpressionValue)
-                    {
-                        // op1 is variable
-                        var dsts = (from var in variables where var.name == identifierExpressionValue.Literal select var);
-                        if (!dsts.Any())
-                        {
-                            throw new Exception("Undefined variable: " + identifierExpressionValue.Literal);
-                        }
-                        result.Add((byte)dsts.First().register);
-                        result.Add((byte)0b01010101);
-                    }
-                    else if (expression is NumberLiteral numberLiteralValue)
-                    {
-                        // op1 is immediate
-                        result.Add((byte)0x00);
-                        result.Add((byte)0b01010110);
-                        result.AddRange(BitConverter.GetBytes(long.Parse(numberLiteralValue.Value)));
-                    }
-                    TempRegister++;
-                };
-                movIntoTempRegister(infixExpression.Left);
-                movIntoTempRegister(infixExpression.Right);
-                if (infixExpression.Operator.Literal == "+")
-                {
-                    result.Add((byte)0x01);
-                }
-                else if (infixExpression.Operator.Literal == "-")
-                {
-                    result.Add((byte)0x02);
-                }
-                else if (infixExpression.Operator.Literal == "*")
-                {
-                    result.Add((byte)0x03);
-                }
-                else if (infixExpression.Operator.Literal == "/")
-                {
-                    result.Add((byte)0x04);
-                }
-                result.AddRange(new byte[] { (byte)lastTempRegister, (byte)(lastTempRegister + 1), 0b01010101 });
-                TempRegister = lastTempRegister; // reset
-            }
-            else if (node is FunctionLiteral functionLiteral)
-            {
-                foreach (var subnode in functionLiteral.Body.Body)
-                {
-                    result.AddRange(compileNode(subnode));
-                }
             } else if (node is CallExpression callExpression)
             {
-                // call指令未完成，先做vmcall吧。
+                bool isVMCall = false;
                 if(callExpression.Function.Literal == "owovm$vmcall")
                 {
                     if(enableVMCall)
                     {
-                        // 启用vmcall了
-                        if(callExpression.Parameters[0] is NumberLiteral vmcallOpcode)
-                        {
-                            result.AddRange(instMovToRegister(48, UInt64.Parse(vmcallOpcode.Value)));
-                        }else
-                        {
-                            throw new Exception("The opcode of VMCall is must number");
-                        }
-                        for (int i = 1; i < callExpression.Parameters.Count; i++)
-                        {
-                            if(callExpression.Parameters[i] is IdentifierExpression vmcallArg)
-                            {
-                                var varname = vmcallArg.Literal;
-                                var resultVars = (from var in variables where var.name == varname select var);
-                                if(!resultVars.Any()) { throw new Exception("Undefined variable: " + varname); }
-                                var inputVar = resultVars.First();
-                                result.AddRange(instMovToRegister((byte)(48 + i), (byte)inputVar.register));
-                            } else if(callExpression.Parameters[i] is NumberLiteral vmcallArgNum)
-                            {
-                                result.AddRange(instMovToRegister((byte)(48 + i), UInt64.Parse(vmcallArgNum.Value)));
-                            } else if(callExpression.Parameters[i] is StringLiteral)
-                            {
-                                result.AddRange(compileNode(callExpression.Parameters[i]));
-                                result.AddRange(new byte[] { 0x0D, (byte)(48 + i), (byte)TempRegister, 00 });
-                            }
-                        }
-                        result.AddRange(new byte[] { 0x0E, 0, 0, 0 });
+                        isVMCall = true;
                     } else
                     {
                         throw new Exception("VMCall is not allowed");
                     }
                 }
+                for (int i = 0; i < callExpression.Parameters.Count; i++)
+                {
+                    if (callExpression.Parameters[i] is IdentifierExpression vmcallArg)
+                    {
+                        moveVariable((byte)(48 + i), vmcallArg.Literal, ref result);
+                    }
+                    else if (callExpression.Parameters[i] is NumberLiteral vmcallArgNum)
+                    {
+                        var value = vmcallArgNum.Value;
+                        result.EmitMov(
+                            new Register((byte)(48 + i)),
+                            new Immediate(GetImmediateType("", value), value)
+                            );
+                    }
+                    else if (callExpression.Parameters[i] is StringLiteral)
+                    {
+                        result.Emit(compileNode(callExpression.Parameters[i], TopRegister, TempRegister));
+                        result.EmitMov(
+                            new Register((byte)(48 + i)),
+                            new Register((byte)(TempRegister)));
+                    }
+                }
+                if(isVMCall)
+                {
+                    result.EmitVMCall();
+                }
             } else if (node is StringLiteral stringLiteral)
             {
                 string parsedString = stringLiteral.Value;
                 parsedString = parsedString.Replace("\\n", "\n");
-                result.AddRange(new byte[] { 0x12, (byte)TempRegister, 0, 0 });
-                result.AddRange(BitConverter.GetBytes(Convert.ToUInt64(parsedString.Length)));
-                result.AddRange(Encoding.Default.GetBytes(parsedString));
+                result.EmitString(
+                    new Register((byte)TempRegister),
+                    parsedString
+                    );
+            } else if (node is FunctionLiteral functionLiteral)
+            {
+                var func = compileNode(functionLiteral.Body);
+                func.EmitRet();
+                result.Emit(func);
+            } else if (node is BlockStatement blockStatement)
+            {
+                foreach(var subnode in blockStatement.Body)
+                {
+                    result.Emit(compileNode(subnode));
+                }
             }
-            return result.ToArray();
+            return result;
         }
         Stack<IndexItem> itemStack = new Stack<IndexItem>();
         // 仅适用于compile结构代码
-        private byte[] compileNodeArray(Node[] nodeArray)
+        private CodeBuffer compileNodeArray(Node[] nodeArray)
         {
-            List<byte> result = new List<byte>();
+            CodeBuffer result = new CodeBuffer();
             foreach (var node in nodeArray)
             {
                 bool compiled = false;
@@ -188,9 +153,9 @@ namespace grasslang.OwoVMCompiler
                     {
                         // TODO: 暂时不支持参数，什么时候支持等我什么时候整完call指令
                         string prefix = itemStack.Count != 0 ? itemStack.Peek().Name + "." : "";
-                        indexItems.Add(new IndexItem(prefix + functionLiteral.FunctionName.Literal + "()", 0x04, Convert.ToUInt64(result.Count)));
+                        indexItems.Add(new IndexItem(prefix + functionLiteral.FunctionName.Literal + "()", 0x04, Convert.ToUInt64(result.Length)));
                         // 0x04 means function/method
-                        result.AddRange(compileNode(node));
+                        result.Emit(compileNode(node));
                         compiled = true;
                     }
                     else if (expressionStatement.Expression is ClassLiteral classLiteral)
@@ -198,7 +163,7 @@ namespace grasslang.OwoVMCompiler
                         var item = new IndexItem(classLiteral.TypeName.Literal, 0x02, 0x00);// 长度0x00空类（等后面加入class的field再写）
                         itemStack.Push(item);
                         indexItems.Add(item);
-                        result.AddRange(compileNodeArray(classLiteral.Body.Body.ToArray()));
+                        result.Emit(compileNodeArray(classLiteral.Body.Body.ToArray()));
                         itemStack.Pop();
                     }
                 }
@@ -207,16 +172,15 @@ namespace grasslang.OwoVMCompiler
                     // TODO: throw an exception here
                 }
             }
-            return result.ToArray();
+            return result;
         }
         /*
             思路： 结构上仅允许class/struct/function
             突然想到还没有class呢，先鸽了(划掉) 草 原来已经有class了，鸽了几个月忘记了
          */
-        private byte[] compileAst(Ast ast)
+        private CodeBuffer compileAst(Ast ast)
         {
-            var result = compileNodeArray(ast.Root.ToArray());
-            return result;
+            return compileNodeArray(ast.Root.ToArray());
         }
         private byte[] compileIndexItems()
         {
@@ -230,7 +194,7 @@ namespace grasslang.OwoVMCompiler
         public byte[] Compile(Ast ast)
         {
             List<byte> result = new List<byte>();
-            byte[] codeResult = compileAst(ast);
+            byte[] codeResult = compileAst(ast).Build();
             byte[] indexResult = compileIndexItems();
             result.AddRange(BitConverter.GetBytes(Convert.ToUInt64(indexResult.Length)));
             result.AddRange(indexResult);
